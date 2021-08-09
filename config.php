2267705a92fb4930bcccc7aec92d4fb4b3b3d8cb
obezya <83490569+obezya@users.noreply.github.com>
2021-08-09 12:20:53 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+	'db' => array(
+		'server' => 'localhost',
+		'username' => 'drbcoder_Roman',
+		'password' => 'Abc901177Abc901177',
+		'name' => 'drbcoder_blog'
+	)
+);
+
+require "db.php";
